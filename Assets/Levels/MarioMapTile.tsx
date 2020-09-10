b3e9b95b08dfc506af9024d90002f80e0b7493c1
Junyoung Kim <jun.gamedev@gmail.com>
2020-09-10 18:40:59 +0900

    Level data

---
+++ Assets/Levels/MarioMapTile.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="MarioMapTile" tilewidth="16" tileheight="16" tilecount="924" columns="33">
+ <image source="../Images/Map/Map_Tileset.png" width="528" height="448"/>
+ <tile id="0">
+  <properties>
+   <property name="RenderingRatio" type="float" value="3"/>
+   <property name="name" value="MarioMapTile"/>
+  </properties>
+ </tile>
+</tileset>
